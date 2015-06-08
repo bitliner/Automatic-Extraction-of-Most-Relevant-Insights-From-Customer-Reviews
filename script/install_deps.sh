cc029199112c0d00dece18225f63b46fd7817407
bitliner <giovanni.gaglione@gmail.com>
2015-06-09 00:20:33 +0200

    +script to install dependencies

---
+++ script/install_deps.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# the following lets you run this script from whatever folder you want without problem of relative/absolute paths
+SCRIPT_HOME="$( cd "$( dirname "$0" )" && pwd )"
+cd $SCRIPT_HOME/..
+
+# main script
+
+function install_os_deps() {
+	echo '---> Installing os dependencies...'
+	sudo apt-get install python-pip
+	sudo apt-get install gfortran libopenblas-dev liblapack-dev
+}
+
+function install_program_deps() {
+	echo '---> Installing dependencies of program...'
+	pip install numpy
+	pip install scipy
+}
+
+install_os_deps && install_program_deps
+
