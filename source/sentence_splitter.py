5de0074f752b9f1db48a186d393fbaa4dde52db2
bitliner <giovanni.gaglione@gmail.com>
2015-06-09 16:05:40 +0200

    added extra splitting for multiple sentences per line using NLTK + better tracking of sentences being processed if log=True

--- source/sentence_splitter.py
+++ source/sentence_splitter.py
@@ -1,3 +1,5 @@
+#!/usr/local/bin/python
+
 __author__ = 'Wisse'
 
 import gensim as gs
@@ -10,6 +12,11 @@ stop = nltk.corpus.stopwords.words('english')
 
 LabeledSentence = gs.models.doc2vec.LabeledSentence
 parser = HTMLParser()
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+#TODO: Splits lines not sentences!
+
+
 
 # iterating object of directory or file
 class MySentences(object):
@@ -17,15 +24,36 @@ class MySentences(object):
         self.name = name
         self.labels = []
         self.size = 0
-        self.lod = True
+        self.log = True
     def __iter__(self):
+        """Iterate through the lines in file or in files in source directory."""
+        self.size = 0
         if os.path.isdir(self.name):
-            """Iterate through the lines in the source directory."""
             for fname in os.listdir(self.name):
-                for item_no, line in enumerate(open(os.path.join(self.name, fname))):
+                for line in open(os.path.join(self.name, fname)):
+                    sentences = tokenizer.tokenize(line)
+                    for item_no, sentence in enumerate(sentences):
+                        # preprocessing: remove HTML, stopwords, numbers; convert to lowercase & unicode
+                        decoded = parser.unescape(sentence).lower()
+                        words = [i for i in gs.utils.to_unicode(decoded).split() if i not in stop and not i.isdigit()]
+                        label = 'SENT_%s' % item_no
 
+                        # store labels
+                        if label not in self.labels:
+                            self.labels.append(label)
+
+                        # log process
+                        self.size += 1
+                        if self.log and self.size % 1000 == 0:
+
+                            print "Processed {0} sentences now.".format(self.size)
+                        yield LabeledSentence(words, [label])
+        else:
+            for line in open(self.name, 'r'):
+                sentences = tokenizer.tokenize(line)
+                for item_no, sentence in enumerate(sentences):
                     # preprocessing: remove HTML, stopwords, numbers; convert to lowercase & unicode
-                    decoded = parser.unescape(line).lower()
+                    decoded = parser.unescape(sentence).lower()
                     words = [i for i in gs.utils.to_unicode(decoded).split() if i not in stop and not i.isdigit()]
                     label = 'SENT_%s' % item_no
 
@@ -34,25 +62,11 @@ class MySentences(object):
                         self.labels.append(label)
 
                     # log process
-                    if self.log == True and self.size % 1000 == 0:
-                        print "The model includes {0} sentences now.".format(self.size)
+                    self.size += 1
+                    if self.log and self.size % 1000 == 0:
+
+                        print "Processed {0} sentences now.".format(self.size)
                     yield LabeledSentence(words, [label])
-        else:
-            for item_no, line in enumerate(open(self.name, 'r')):
-                # preprocessing: remove HTML, stopwords, numbers; convert to lowercase & unicode
-                decoded = parser.unescape(line).lower()
-                words = [i for i in gs.utils.to_unicode(decoded).split() if i not in stop and not i.isdigit()]
-                label = 'SENT_%s' % item_no
-
-                # store labels
-                if label not in self.labels:
-                    self.labels.append(label)
-
-                # log process
-                if self.log == True and self.size % 1000 == 0:
-                    print "The model includes {0} sentences now.".format(self.size)
-
-                yield LabeledSentence(words, [label])
 
 
 
