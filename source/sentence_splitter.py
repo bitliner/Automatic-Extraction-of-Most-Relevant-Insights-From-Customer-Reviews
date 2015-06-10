41e8fa676efafd645cb4ed89f31b63fd5b28c3b7
bitliner <giovanni.gaglione@gmail.com>
2015-06-10 10:36:05 +0200

    fixed unicode bug

--- source/sentence_splitter.py
+++ source/sentence_splitter.py
@@ -31,11 +31,11 @@ class MySentences(object):
         if os.path.isdir(self.name):
             for fname in os.listdir(self.name):
                 for line in open(os.path.join(self.name, fname)):
-                    sentences = tokenizer.tokenize(line)
+                    sentences = tokenizer.tokenize(gs.utils.to_unicode(line))
                     for item_no, sentence in enumerate(sentences):
                         # preprocessing: remove HTML, stopwords, numbers; convert to lowercase & unicode
                         decoded = parser.unescape(sentence).lower()
-                        words = [i for i in gs.utils.to_unicode(decoded).split() if i not in stop and not i.isdigit()]
+                        words = [i for i in decoded.split() if i not in stop and not i.isdigit()]
                         label = 'SENT_%s' % item_no
 
                         # store labels
@@ -50,11 +50,11 @@ class MySentences(object):
                         yield LabeledSentence(words, [label])
         else:
             for line in open(self.name, 'r'):
-                sentences = tokenizer.tokenize(line)
+                sentences = tokenizer.tokenize(gs.utils.to_unicode(line))
                 for item_no, sentence in enumerate(sentences):
                     # preprocessing: remove HTML, stopwords, numbers; convert to lowercase & unicode
                     decoded = parser.unescape(sentence).lower()
-                    words = [i for i in gs.utils.to_unicode(decoded).split() if i not in stop and not i.isdigit()]
+                    words = [i for i in decoded.split() if i not in stop and not i.isdigit()]
                     label = 'SENT_%s' % item_no
 
                     # store labels
