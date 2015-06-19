3a6e09f2d08d2c27451c9eacf33844d7dab0f3d6
bitliner <giovanni.gaglione@gmail.com>
2015-06-19 14:44:10 +0200

    parse arguments

--- source/test.py
+++ source/test.py
@@ -6,12 +6,12 @@ import extract
 import evaluate
 import sys
 
-test_data_path = sys.argv[1]
-model_path = sys.argv[2]
-exp_name = sys.argv[3]
+test_data_path = str(sys.argv[1])
+model_path = str(sys.argv[2])
+exp_name = str(sys.argv[3])
 
 vector_size = int(sys.argv[4])
-save = sys.argv[5]
+save = bool(sys.argv[5])
 level = int(sys.argv[6])
 
 data_vecs = get_vecs.get_data_vecs(model_path, test_data_path, vector_size)
