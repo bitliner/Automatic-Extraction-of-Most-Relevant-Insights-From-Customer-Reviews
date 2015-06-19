c7ec5de14c62d5f1908b043b4380bcbfa915463e
bitliner <giovanni.gaglione@gmail.com>
2015-06-19 14:15:16 +0200

    put the everyting into functions, created test.py to execute the whole test-pipeline at once

---
+++ source/test.py
@@ -0,0 +1,23 @@
+__author__ = 'Wisse'
+
+import dendrogram
+import get_vecs
+import extract
+import evaluate
+import sys
+
+test_data_path = sys.argv[1]
+model_path = sys.argv[2]
+exp_name = sys.argv[3]
+
+vector_size = sys.argv[4]
+save = sys.argv[5]
+level = sys.argv[6]
+
+data_vecs = get_vecs.get_data_vecs(model_path, test_data_path, vector_size)
+
+links = extract.cluster(data_vecs, exp_name, save)
+
+nmi = evaluate.evaluate(test_data_path, links, save, level)
+
+
