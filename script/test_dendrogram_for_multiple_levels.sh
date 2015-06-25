69ca9a2c69fc6ac45003dda2023161f585052fd8
bitliner <giovanni.gaglione@gmail.com>
2015-06-25 12:00:11 +0200

    edti input args

--- script/test_dendrogram_for_multiple_levels.sh
+++ script/test_dendrogram_for_multiple_levels.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-# 1 = data
-# 2 = output folder
-# 3 = levels
+# $1 = data
+# $2 = model
+# $3 = output folder
+# $4 = levels
 
-mkdir results/$2/
-
-for i in `seq 1 $3`; do
-python source/test_dendrogram.py $1 models/large_training/model400.doc2vec $2 400 $i 0 $2 1 20;
+for i in `seq 1 $4`; do
+python source/test_dendrogram.py $1 $2 $3 400 $i 0 $3 1 20;
 done
